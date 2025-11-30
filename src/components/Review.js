11c4f1b80ef633b2a02bdb9f8290178f86e1370c
= <sagarsoni32896@gmail.com>
2025-11-30 16:48:16 +0530

    v5

--- src/components/Review.js
+++ src/components/Review.js
@@ -2,19 +2,37 @@ import React, { useState } from 'react'
 import data from "../data.js"
 
 function Review() {
-    let[c,setc] = useState(0)
+  let [c, setc] = useState(0)
+
+  const prev = () => {
+    setc((c - 1 + data.length) % data.length)
+  }
+
+  const next = () => {
+    setc((c + 1) % data.length)
+  }
+
+  const random = () => {
+    let rand = Math.floor(Math.random() * data.length)
+    while (rand === c) {
+      rand = Math.floor(Math.random() * data.length)
+    }
+    setc(rand)
+  }
+
   return (
     <div className="review">
-      
-      <p className='author' id={`author-${data[c].id}`} ><a>{data[c].name}</a></p>
-      
+      <p className='author' id={`author-${data[c].id}`}>
+        {data[c].name}
+      </p>
+
       <p className='job'>{data[c].job}</p>
       <p className='info'>{data[c].text}</p>
-      <img className='person-img' style={{height:"244px",width:"244px"}}src={data[c].image}/>
+      <img className='person-img' style={{height:"244px", width:"244px"}} src={data[c].image}/>
 
-      <button className="prev-btn" onClick={()=>setc(c>0?c-1:c)}>Previous</button>
-      <button className="next-btn" onClick={()=>setc(c<data.length-1?c+1:c)}>Next</button>
-      <button className='random-btn' onClick={()=>setc(parseInt(Math.random()*4))}>surprise me</button>
+      <button className="prev-btn" onClick={prev}>Previous</button>
+      <button className="next-btn" onClick={next}>Next</button>
+      <button className='random-btn' onClick={random}>surprise me</button>
     </div>
   )
 }
